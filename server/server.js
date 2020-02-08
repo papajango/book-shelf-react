87b997a3db33e2fc30b90c65727de2b3b23c3d4e
kondakovdi <kondakovdi@iMac-2.local>
2020-02-08 15:53:39 +0300

    installed npm packages and started writing server part

---
+++ server/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const config = require("./config/config").get(process.env.NODE_ENV);
+const app = express();
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.DATABASE, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
+
+const { User } = require("./models/user");
+const { Book } = require("./models/book");
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.post("/api/book", (req, res) => {
+	const book = new Book(req.body);
+	book.save((err, doc) => {
+		if (err) return res.status(400).send(err);
+		res.status(200).json({
+			post: true,
+			bookId: doc._id
+		});
+	});
+});
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+	console.log("Server running");
+});
