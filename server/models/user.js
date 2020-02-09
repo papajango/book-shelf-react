6bee71e35700a2be6d88303fbb729ec18f5b4a0c
kondakovdi <kondakovdi@iMac-2.local>
2020-02-09 11:58:15 +0300

    finished server routes

--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,7 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const config = require('../config/config').get(process.env.NODE_ENV);
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const config = require("../config/config").get(process.env.NODE_ENV);
 const SALT_I = 10;
 
 const userSchema = mongoose.Schema({
@@ -33,9 +33,9 @@ const userSchema = mongoose.Schema({
 	}
 });
 
-userSchema.pre('save', function(next) {
+userSchema.pre("save", function(next) {
 	var user = this;
-	if (user.isModified('password')) {
+	if (user.isModified("password")) {
 		bcrypt.genSalt(SALT_I, function(err, salt) {
 			if (err) return next(err);
 			bcrypt.hash(user.password, salt, function(err, hash) {
@@ -49,5 +49,40 @@ userSchema.pre('save', function(next) {
 	}
 });
 
-const User = mongoose.model('User', userSchema);
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+	bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+		if (err) return cb(err);
+		cb(null, isMatch);
+	});
+};
+
+userSchema.methods.generateToken = function(cb) {
+	var user = this;
+	var token = jwt.sign(user._id.toHexString(), config.SECRET);
+	user.token = token;
+	user.save(function(err, user) {
+		if (err) return cb(err);
+		cb(null, user);
+	});
+};
+
+userSchema.statics.findByToken = function(token, cb) {
+	var user = this;
+	jwt.verify(token, config.SECRET, function(err, decode) {
+		user.findOne({ _id: decode, token: token }, function(err, user) {
+			if (err) return cb(err);
+			cb(null, user);
+		});
+	});
+};
+
+userSchema.methods.deleteToken = function(token, cb) {
+	var user = this;
+	user.update({ $unset: { token: 1 } }, (err, user) => {
+		if (err) return cb(err);
+		cb(null, user);
+	});
+};
+
+const User = mongoose.model("User", userSchema);
 module.exports = { User };
